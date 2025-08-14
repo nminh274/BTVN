1b2e377ff2587b099bd8b0dd4d61e7ca0455809e
Kuro Tran <nminhtran274@gmail.com>
2025-08-14 08:15:29 +0700

    Add files via upload

---
+++ FC1_TranNhatMinh/src/LEC/ex1.java
@@ -0,0 +1,19 @@
+package LEC;
+
+public class ex1
+{
+    public static void main(String[] args) {
+        int a = 1 + 2;
+        double b = 3.5 + 8;
+        double c = 1/2;
+        double d = 4.5 + 7;
+        int e = 4 * 5 + 1;
+        double f = 20 % 3 + 2 - 7;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+    }
+}
---
+++ FC1_TranNhatMinh/src/LEC/ex21.java
@@ -0,0 +1,10 @@
+package LEC;
+
+public class ex21 {
+    public static void main(String[] args) {
+    int x = 1;
+    int y = x + ++x - x-- - x++ + x - x--;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+    }
+}
